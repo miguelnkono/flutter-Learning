5a54cf2c920be6eade06bd6928bfd40144ad80e6
Miguel <nkonondememiguel7@gmail.com>
2024-09-11 09:07:26 +0200

    Starting the implementation of the icons navigation.

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -9,43 +9,49 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  final appScreens = [
+    Center(child: const Text("Home")),
+    Center(child: const Text("Searh")),
+    Center(child: const Text("Tickets")),
+    Center(child: const Text("Profile"))
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      appBar: AppBar(
-        title: Text("My tickets"),
-      ), // AppBar
-      body: Center(
-        child: Text("Tickets info"),
-      ), // Center
-      bottomNavigationBar:BottomNavigationBar(
-        selectedItemColor: Colors.blueGrey, // set the selected item's color
-        showSelectedLabels: false,
-        unselectedItemColor: const Color(0xFF526400),   // set the unselected item's color
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-                  // icon: Icon(Icons.home),
-            label: "Home"
-          ), // BottomNavigationBarItem: home
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-            label: "Search"
-          ), // BottomNavigationBarItem: search
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_airplane_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_airplane_filled), 
-            label: "Tickets"
-          ), // BottomNavigationBarItem: airplane_tocket
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled), 
-            label: "Profile"
-          )  // BottomNavigationBarItem: person
-        ],
-      ) // BottomNavigationBar
-    ); // Scaffold
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("My tickets"),
+        ), // AppBar
+        
+        body: Center(
+          child: Text("Tickets info"),
+        ), // Center
+
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueGrey, // set the selected item's color
+          showSelectedLabels: false,
+          unselectedItemColor:
+              const Color(0xFF526400), // set the unselected item's color
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                // icon: Icon(Icons.home),
+                label: "Home"), // BottomNavigationBarItem: home
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: "Search"), // BottomNavigationBarItem: search
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_airplane_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_airplane_filled),
+                label: "Tickets"), // BottomNavigationBarItem: airplane_tocket
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                label: "Profile") // BottomNavigationBarItem: person
+          ],
+        ) // BottomNavigationBar
+        ); // Scaffold
   }
 }
